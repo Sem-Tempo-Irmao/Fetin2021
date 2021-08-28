cd573b4e4e5511116589cde9aaea557d6e4d9b6a
matheuslaguiar <matheuslemosaguiar@hotmail.com>
2021-08-28 14:18:02 -0300

    Add mapa na tela inicial

--- front-end/front_end_local_farmaco/lib/pages/HomePage.dart
+++ front-end/front_end_local_farmaco/lib/pages/HomePage.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+import 'dart:ui';
 import 'package:flutter/material.dart';
+
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -19,18 +24,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Completer<GoogleMapController> _controller = Completer();
+  TextEditingController _pesq = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(-22.256897, -45.7011926),
+    zoom: 14.4746,
+  );
+  
+
 
   @override
   Widget build(BuildContext context) {
@@ -92,41 +94,71 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
 
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Você pressionou o botão essa quantidade de vzs:',
+      body: ListView(
+        padding: EdgeInsets.only(top: 20, left: 20, right: 10),
+        children: <Widget> [
+          Center(
+            child:
+            // Center is a layout widget. It takes a single child and positions it
+            // in the middle of the parent.
+            Column(
+              // Column is also a layout widget. It takes a list of children and
+              // arranges them vertically. By default, it sizes itself to fit its
+              // children horizontally, and tries to be as tall as its parent.
+              //
+              // Invoke "debug painting" (press "p" in the console, choose the
+              // "Toggle Debug Paint" action from the Flutter Inspector in Android
+              // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+              // to see the wireframe for each widget.
+              //
+              // Column has various properties to control how it sizes itself and
+              // how it positions its children. Here we use mainAxisAlignment to
+              // center the children vertically; the main axis here is the vertical
+              // axis because Columns are vertical (the cross axis would be
+              // horizontal).
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextField(
+                  controller: _pesq,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Pesquisa'
+                  ),
+                  onSubmitted: (String value){
+                    // faz a busca pelo "value"
+                  },
+                ),
+                Container(
+                  height: 20,
+                ),
+                Text(
+                  'Digite o que deseja procurar no espaço acima.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                  ),
+                ),
+                Container(
+                  height: 20,
+                ),
+                SizedBox(
+                  height: 400,
+                  width: 400,
+                  child: GoogleMap(
+                    liteModeEnabled: true,
+                    mapType: MapType.normal,
+                    initialCameraPosition: _kGooglePlex,
+                    onMapCreated: (GoogleMapController controller) {
+                      _controller.complete(controller);
+                    },
+                  ),
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
